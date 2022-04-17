7d9d07d308cb4c6538012c75f22d953e1a2111c9
Samuel-ACH <samuelantoniocastro2021@gmail.com>
2022-04-17 12:34:16 -0600

    Hospital

---
+++ Datos/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,25 @@
+﻿using Datos.Interfaces;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Repositorio
+{
+   public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private string CadenaConexion;
+
+        public UsuarioRepositorio(string cadenaConexion)
+        {
+            CadenaConexion = cadenaConexion;
+        }
+
+        private MySqlConnection Conexion()
+        {
+            return new MySqlConnection(CadenaConexion);
+        }
+    }
+}
---
+++ Modelos/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage = "El Campo Codigo es Obligatorio")]
+        public string Clave { get; set; }
+        [Required(ErrorMessage = "El Campo Nombre es Obligatorio")]
+        public string Nombre { get; set; }
+       
+    }
+}
